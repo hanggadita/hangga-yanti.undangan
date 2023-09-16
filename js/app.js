7d7c77ff4ff82a8c8ed177fa39b3317986b17c4d
hanggaditasuryasetyaji <57597274+hanggadita@users.noreply.github.com>
2023-09-16 09:50:11 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -560,6 +560,8 @@ window.addEventListener('load', () => {
         div.innerHTML = `
         <p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p>
         <h2 class="text-light">${escapeHtml(name)}</h2>
+        <img src="/qr/${name}.png" alt="">
+        <p class="mt-0 mb-1 mx-0 p-0 text-light">${name}</p>
         `;
 
         document.getElementById('formnama').value = name;
