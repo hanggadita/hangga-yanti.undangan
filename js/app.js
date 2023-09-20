966b8c2935ebc5ef9c08afe24a491fcd04245784
hanggaditasuryasetyaji <57597274+hanggadita@users.noreply.github.com>
2023-09-20 13:11:03 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ const audio = (() => {
 
 const escapeHtml = (unsafe) => {
     return unsafe
+       .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
@@ -554,20 +555,18 @@ const kirim = async () => {
 
 window.addEventListener('load', () => {
     let modal = new bootstrap.Modal('#exampleModal');
-    let nameParam = (new URLSearchParams(window.location.search)).get('to') ?? '';
-    let decodedNameParam = decodeURIComponent(nameParam);
-    let name = (decodedNameParam && decodedNameParam.length > 0) ? decodedNameParam : '';
+    let name = (new URLSearchParams(window.location.search)).get('to') ?? '';
 
     if (name.length == 0) {
         document.getElementById('namatamu').remove();
     } else {
+        alert(name);
         let div = document.createElement('div');
         div.classList.add('m-2');
         div.innerHTML = `
         <p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p>
         <h2 class="text-light">${escapeHtml(name)}</h2>
-        <img src="/imgqr/${encodeURIComponent(name)}.png" style="max-width: 200px" alt="">
-        <p class="mt-0 mb-1 mx-0 p-0 text-light">${escapeHtml(name)} | ${nameParam}.png</p>
+        <img src="/imgqr/${name}.png" style="max-width: 200px" alt="">
         `;
 
         document.getElementById('formnama').value = name;
