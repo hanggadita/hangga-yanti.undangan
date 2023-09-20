5e10b45d04e742ee05a856bfcb085918b9cb744b
hanggaditasuryasetyaji <57597274+hanggadita@users.noreply.github.com>
2023-09-20 15:56:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ const audio = (() => {
 
 const escapeHtml = (unsafe) => {
     return unsafe
-       .replace(/&/g, '%26;')
+       .replace(/&/g, '$amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
